db7e7195c7bfcb5f757f8e19da60b97f91a20249
AjdinBajric <41507174+AjdinBajric@users.noreply.github.com>
2018-08-07 22:46:51 +0200

    Added source code

---
+++ 05_CreatingAlertBoxes/AlertBox.java
@@ -0,0 +1,34 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class AlertBox {
+
+	public static void display(String title, String message) {
+
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Alert!");
+		window.setMinWidth(250);
+
+		Label label = new Label();
+		label.setText(message);
+
+		Button btn = new Button("OK");
+		btn.setOnAction(e -> window.close());
+
+		VBox layout = new VBox();
+		layout.getChildren().addAll(label, btn);
+		layout.setAlignment(Pos.CENTER);
+
+		Scene scene = new Scene(layout);
+		window.setScene(scene);
+		window.showAndWait();
+
+	}
+
+}
